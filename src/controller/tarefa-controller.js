0b5b78d119061e9e3346df6ab276ce35044db934
cinmcantu <cinthiamcantu@gmail.com>
2022-07-14 21:23:20 -0300

    criando rota post

--- src/controller/tarefa-controller.js
+++ src/controller/tarefa-controller.js
@@ -1,7 +1,18 @@
+// funçao que vai receber a instancia do servidor como parametros
+// e vai agrupar todos metodos que representam as rotas
+
 const tarefaController = (app)=>{
+    // Rotas com mesmo caminho ('/tarefa'), mas com verbos diferentes
+    // são rotas diferentes
+
     app.get('/tarefa', (req, res)=>{
         res.send("Rota GET para o tarefa")
     })
+
+    app.post('/tarefa', (req, res)=>{
+        res.send("Rota POST para usuario")
+    })
 }
 
+// exportando a função
 export default tarefaController
--- src/controller/usuario-controller.js
+++ src/controller/usuario-controller.js
@@ -1,7 +1,18 @@
+// funçao que vai receber a instancia do servidor como parametros
+// e vai agrupar todos metodos que representam as rotas
+
 const usuarioController = (app)=>{
+    // Rotas com mesmo caminho ('/usuario'), mas com verbos diferentes
+    // são rotas diferentes
+
     app.get('/usuario', (req, res)=>{
         res.send("Rota GET para o usuario")
     })
+
+    app.post('/usuario', (req, res)=>{
+        res.send("Rota POST para usuario")
+    })
 }
 
+// exportando a função
 export default usuarioController
